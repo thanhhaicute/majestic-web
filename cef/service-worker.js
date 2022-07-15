8c700725de3ad2a3823de5d2837eddb26c547cb9
Derry <thanhhaivevo@gmail.com>
2022-07-15 13:59:58 +0700

    awfW

--- cef/service-worker.js
+++ cef/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.2b9cf42af25bd7db6556b88e15a72a32.js"
+  "./precache-manifest.3d8c58632cdab80bfd336382114815d8.js"
 );
 
 self.addEventListener('message', (event) => {
